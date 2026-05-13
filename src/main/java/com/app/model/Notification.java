3f40d7ebd3cbc137f069c01787273a06152065d4
diana-dev <diana-dev@pipelinehub.dev>
2026-05-14 04:23:21 +0530

    feat: add notification model

---
+++ src/main/java/com/app/model/Notification.java
@@ -0,0 +1,20 @@
+package com.app.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification {
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("timestamp")
+    private Long timestamp;
+}

c5cac8d9a1c59d832c2169b6e474478dfbbe7742
diana-dev <diana-dev@pipelinehub.dev>
2026-05-14 09:53:42 +0530

    feat: add notification utility functions

---
+++ src/main/java/com/app/util/NotificationUtil.java
@@ -0,0 +1,16 @@
+package com.app.util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class NotificationUtil {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+    public static String formatTimestamp(LocalDateTime dateTime) {
+        return dateTime.format(FORMATTER);
+    }
+
+    public static LocalDateTime parseTimestamp(String timestamp) {
+        return LocalDateTime.parse(timestamp, FORMATTER);
+    }
+}
